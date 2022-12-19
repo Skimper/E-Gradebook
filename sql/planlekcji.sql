36d702d6b0ad8e5f546fdb6e47a290d7048ba5f8
skimper <skimpertm@o2.pl>
2022-12-19 03:20:59 +0100

    last fix

--- sql/planlekcji.sql
+++ sql/planlekcji.sql
@@ -1,74 +1,74 @@
-INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',3,'2022-12-19','1');
-INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',3,'2022-12-19','2');
+INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',3,'2022-12-12','1');
+INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',3,'2022-12-12','2');
 
-INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',3,'2022-12-20','6');
-INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',3,'2022-12-20','7');
+INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',3,'2022-12-13','6');
+INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',3,'2022-12-13','7');
 
-INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',3,'2022-12-21','1');
-INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',3,'2022-12-21','4');
+INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',3,'2022-12-14','1');
+INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',3,'2022-12-14','4');
 
-INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',4,'2022-12-20','5');
+INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',4,'2022-12-13','5');
 
-INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',4,'2022-12-22','1');
-INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',4,'2022-12-22','7');
-INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',4,'2022-12-22','8');
-
-
-INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',5,'2022-12-23','1');
-INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',5,'2022-12-23','2');
+INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',4,'2022-12-15','1');
+INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',4,'2022-12-15','7');
+INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',4,'2022-12-15','8');
 
 
-INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',6,'2022-12-19','3');
-INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',6,'2022-12-19','4');
-
-INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',6,'2022-12-20','1');
-
-INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',6,'2022-12-21','6');
-INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',6,'2022-12-21','7');
-
-INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',6,'2022-12-22','2');
+INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',5,'2022-12-16','1');
+INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',5,'2022-12-16','2');
 
 
-INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',7,'2022-12-21','2');
+INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',6,'2022-12-12','3');
+INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',6,'2022-12-12','4');
 
-INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',7,'2022-12-21','3');
+INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',6,'2022-12-13','1');
 
+INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',6,'2022-12-14','6');
+INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',6,'2022-12-14','7');
 
-INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',8,'2022-12-19','5');
-
-INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',8,'2022-12-22','6');
-
-INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',8,'2022-12-23','3');
-
-
-INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',9,'2022-12-20','2');
-
-INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',9,'2022-12-23','6');
+INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',6,'2022-12-15','2');
 
 
-INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',10,'2022-12-19','6');
+INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',7,'2022-12-14','2');
 
-INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',10,'2022-12-19','7');
-
-INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',10,'2022-12-20','3');
-
-INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',10,'2022-12-20','4');
+INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',7,'2022-12-14','3');
 
 
-INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',11,'2022-12-22','3');
+INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',8,'2022-12-12','5');
+
+INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',8,'2022-12-15','6');
+
+INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',8,'2022-12-16','3');
 
 
-INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',12,'2022-12-22','4');
+INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',9,'2022-12-13','2');
+
+INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',9,'2022-12-16','6');
 
 
-INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',13,'2022-12-21','5');
+INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',10,'2022-12-12','6');
 
-INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',13,'2022-12-23','4');
+INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',10,'2022-12-12','7');
+
+INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',10,'2022-12-13','3');
+
+INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',10,'2022-12-13','4');
 
 
-INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',14,'2022-12-22','5');
-
-INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',14,'2022-12-23','5');
+INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',11,'2022-12-15','3');
 
 
-INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',15,'2022-12-23','7');
+INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',12,'2022-12-15','4');
+
+
+INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',13,'2022-12-14','5');
+
+INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',13,'2022-12-16','4');
+
+
+INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',14,'2022-12-15','5');
+
+INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',14,'2022-12-16','5');
+
+
+INSERT INTO `timetable`(`id`, `classes_id`, `subject_id`, `day`, `lesson`) VALUES (null,'4c',15,'2022-12-16','7');
